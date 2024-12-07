259e9e4400a7a03d94dac122fa0d756160ca3a4b
Micheal <michealobarewon2015@gmail.com>
2024-12-08 00:00:38 +0100

    remove title react icon

--- src/Title.jsx
+++ src/Title.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { FaShieldDog } from 'react-icons/fa6';
 
 const Title = () => {
   return (
     <header>
-      {<FaShieldDog className="logo" />}
       <h1>Furever Friends</h1>
     </header>
   );
