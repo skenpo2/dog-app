bc70ed2a815f32d893afece569427d9dc3f2cd20
Micheal <michealobarewon2015@gmail.com>
2024-12-08 08:53:40 +0100

    title logo changed

--- src/Title.jsx
+++ src/Title.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { FaDog } from 'react-icons/fa'; // Example for a FontAwesome dog icon
 
 const Title = () => {
   return (
     <header>
+      {<FaDog className="logo" />}
       <h1>Furever Friends</h1>
     </header>
   );
