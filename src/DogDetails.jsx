f4707d0c59fc04952e1a3ff81e3563982e903ae9
Micheal <michealobarewon2015@gmail.com>
2024-12-08 12:18:58 +0100

    fixing the tap highlight

---
+++ src/DogDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DogDetails = () => {
+  return (
+    <article className="details-container">
+      <div className="details-image"></div>
+      <div className="details-info"></div>
+    </article>
+  );
+};
+
+export default DogDetails;
